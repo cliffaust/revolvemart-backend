2ff0bf71390b6cb3e4616c3e3cb45457a3ba12f6
Clifford <kingsleyclifford2016@gmail.com>
2021-09-14 19:50:28 +0000

    changed debug to false

--- revolvemart/settings.py
+++ revolvemart/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "k30J0tkbidtppRmLKoZQqFLeZ7hvDLS6rHo6KMBsrBw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["134.122.115.65", "localhost"]
 
