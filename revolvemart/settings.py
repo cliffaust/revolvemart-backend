8c68617514b5f1990d502e78c20ec2915e5281c9
Clifford <kingsleyclifford2016@gmail.com>
2021-09-14 19:33:14 +0000

    initial commit

--- revolvemart/settings.py
+++ revolvemart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["revolvemart-backend.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["134.122.115.65", "localhost"]
 
 
 # Application definition
