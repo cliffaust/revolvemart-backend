12adab67b240eb3dca5f1c5c65daad9136117123
Clifford <kingsleyclifford2016@gmail.com>
2021-09-15 01:09:00 +0000

    changed the database

--- revolvemart/settings.py
+++ revolvemart/settings.py
@@ -113,24 +113,24 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'revolvemart',
-            'USER': 'revolvemart_admin',
-            'PASSWORD': 'clifford2018',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'revolvemart',
+        'USER': 'revolvemart_admin',
+        'PASSWORD': 'clifford2018',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
