92588570e23d9b0ffe1cad2bd3ce5bcdb181597f
Clifford <kingsleyclifford2016@gmail.com>
2021-09-15 02:31:06 +0000

    added line of code

--- revolvemart/settings.py
+++ revolvemart/settings.py
@@ -120,23 +120,18 @@ CORS_ALLOW_ALL_ORIGINS = True
 #             "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
 #         }
 #     }
-DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
 # else:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'revolvemart',
-#         'USER': 'revolvemart_admin',
-#         'PASSWORD': 'clifford2018',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'revolvemart',
+        'USER': 'revolvemart_admin',
+        'PASSWORD': 'clifford2018',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
